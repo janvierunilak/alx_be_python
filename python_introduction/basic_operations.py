93ebd92aeb8021548c35681f35b435b500b30602
Janvierunilak <janvier.rutagengwa@gmail.com>
2025-07-21 22:18:36 +0200

    The basic arthmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1=10
+number2=5
+sum=number1+number2
+product=number1*number2
+difference=number1-number2
+print("The addition of ",number1,"and ",number2, "is",sum)
+print("The subtraction of ",number1, "and ",number2, "is ",difference)
+print("The multiplication of ",number1, "and ",number2, "is ",product)
