4b4af980056d3511a4088b76f2ac6658ebc46e72
Janvierunilak <janvier.rutagengwa@gmail.com>
2025-07-22 23:22:34 +0200

    Finance app working perfectly with a good prompt

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,4 +1,4 @@
-monthly_income=int(input("Enter your total monthly income: "))
+monthly_income=int(input("Enter your monthly income:"))
 monthly_expenses=int(input("Enter your total monthly expenses: "))
 monthly_savings=monthly_income-monthly_expenses
 projected_savings=(monthly_savings *12 +(monthly_savings* 12 * 0.05))
