abfce3312aac695b7a3ae8b4f37b3cb0020efaa5
Janvierunilak <janvier.rutagengwa@gmail.com>
2025-07-23 10:28:51 +0200

    verifying age to check vote eligibility

---
+++ python_introduction/vote_age_checker.py
@@ -0,0 +1,5 @@
+age=int(input("Enter your age:"))
+if age>=18:
+    print("You are eligible to vote in this upcoming elections!")
+else:
+    print("You are not eligible to vote in this upcoming elections!")
