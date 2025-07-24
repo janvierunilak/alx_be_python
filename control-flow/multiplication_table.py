e862adeb71bd73ec8d8d46a56ad49cf90b51e6dc
Janvierunilak <janvier.rutagengwa@gmail.com>
2025-07-24 18:09:54 +0200

    Performing multiplication using for loop

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+number=int(input("Enter a number to see its multiplication table :"))
+
+for numb in range(1,11):
+    product=number*numb
+    print(f" {number } * {numb } = {product}")
+    print()
