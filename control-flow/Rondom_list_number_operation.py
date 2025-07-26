c4b69c2032b87daac09b93373c9a0ac4dfcbc98a
Janvierunilak <janvier.rutagengwa@gmail.com>
2025-07-26 12:40:19 +0200

    Random numbers generation

--- control-flow/Rondom_list_number_operation.py
+++ control-flow/Rondom_list_number_operation.py
@@ -1,13 +1,15 @@
 import random
-
 # Generate a list of 20 random numbers between 1 and 10 (with possible duplicates)
-random_numbers = [random.randint(1, 10) for _ in range(20)]
 
-print("Original list with possible duplicates:")
-print(random_numbers)
+random_numbers_list= [random.randint(1,10) for _ in range (20)]
+print(" Original list with possible duplicates : ")
+print(" 20 random numbers on the list with duplicates :>",random_numbers_list)
 
-# Use set to remove duplicates
-unique_numbers = set(random_numbers)
+unique_numbers_on_the_list= set(random_numbers_list)
+print("\n Unique numbers (duplicates removed): ")
+print(" Unique numbers on the list without duplicates are :> ",unique_numbers_on_the_list)
+print("The Total unique numbers are >> ",len(unique_numbers_on_the_list))
 
-print("\nUnique numbers (duplicates removed):")
-print(unique_numbers)
+
+
+
