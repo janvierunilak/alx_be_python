ff23d0983a2aee4ad8951cc7c8bb137b167b982b
Janvierunilak <janvier.rutagengwa@gmail.com>
2025-07-26 12:39:09 +0200

    http requests handling

--- control-flow/Http_request_handling.py
+++ control-flow/Http_request_handling.py
@@ -1,12 +1,12 @@
 def http_error(status):
     match status :
-        case 400|404|401|402:
+        case 400|404|401:
             print("Bad request")
         case 200:
             print("Successfully")
         case 418:
-            print("Bad gateway")
+            print(" Bad gateway")
         case _ :
             print("Something wrong with the internet <:> ") 
 
-http_error(2000)                   
+http_error(200)                   
