9b9303c2011a7e8c40c8ff65f13a27155fa1b10c
Janvierunilak <janvier.rutagengwa@gmail.com>
2025-08-17 13:34:36 +0200

    Calculator app for basic arithmetic operations

---
+++ control-flow/PYTHON_GEN_EXERCISES/calculator.py
@@ -0,0 +1,14 @@
+def add (number1,number2):
+    return number1 +number2
+def subtract(number1,number2):
+    return number1-number2
+def multiply(number1,number2):
+    return number1*number2
+def division(number1,number2):
+    if number2==0:
+        ZeroDivisionError
+        print("You can not divide by zero! ")
+    else:
+        return number1/number2
+    
+
--- control-flow/python_list_file.py
+++ control-flow/python_list_file.py
@@ -1,3 +1,3 @@
-fruit_list =["Apple","Watermellon", "Avocado", "Banana","Mango"]
-element_access=fruit_list[1]
+fruit_list =["Apple "," Watermellon", "Avocado", " Banana"," Mango"]
+element_access= fruit_list[1]
 print(f" The element at position 2 is : {element_access}")
