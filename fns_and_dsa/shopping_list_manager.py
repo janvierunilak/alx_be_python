939598574e1810ff6283fee07ea0c1bf5c539ce3
Janvierunilak <janvier.rutagengwa@gmail.com>
2025-08-23 11:44:30 +0200

    Item to add resolved

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -15,7 +15,7 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item_to_add=input("Enter Item to add to the list :")
+            item_to_add=input("Enter the item to add:")
             shopping_list.append(item_to_add)
             print(f" >> {item_to_add} >> has been added to the list successfuly !")
             pass
