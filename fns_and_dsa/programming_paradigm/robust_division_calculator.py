9129009acf78107abd2033bc306ba544a8fdbe0e
Janvierunilak <janvier.rutagengwa@gmail.com>
2025-08-26 23:12:24 +0200

    Robust division calculator module

---
+++ fns_and_dsa/programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,15 @@
+
+def safe_divide(numerator, denominator):
+
+    try:
+        num = float(numerator)
+        den = float(denominator)
+
+        if den == 0:
+            return "Error: Cannot divide by zero."
+        else:
+            result = num / den
+        return f"The result of division is {result}"
+
+    except ValueError:
+        return "Error: Please enter numeric values only."
